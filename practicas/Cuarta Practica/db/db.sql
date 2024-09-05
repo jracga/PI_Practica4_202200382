456a57d322247fbe82e88998eb0568e1dcb8780b
Josué <3019975430101@ingenieria.usac.edu.gt>
2024-09-04 19:40:09 -0600

    Back y Front carga

---
+++ practicas/Cuarta Practica/db/db.sql
@@ -0,0 +1,15 @@
+
+CREATE DATABASE practicas;
+
+USE practicas;
+
+CREATE TABLE items (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    description TEXT
+);
+
+
+select * from items;
+
+DROP DATABASE practicas;
